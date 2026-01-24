05da3ba0d129f4ec1c1c77f4d505fd147e27d8a2
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2026-01-24 16:31:56 -0500

    more stuff in dataset and console cleanup

--- main.py
+++ main.py
@@ -9,7 +9,23 @@ data = [
     {"text": "An orange is a type of citrus fruit."},
     {"text": "Formula 1 is the pinnacle of motorsport."},
     {"text": "The ocean covers most of the Earth's surface."},
-    {"text": "Apples are a popular and healthy snack."}
+    {"text": "Apples are a popular and healthy snack."},
+    {"text": "Mountains are majestic landforms that rise prominently above their surroundings."},
+    {"text": "Soccer is the most popular sport worldwide."},
+    {"text": "Bananas are rich in potassium and fiber."},
+    {"text": "The desert is characterized by its arid conditions and sparse vegetation."},
+    {"text": "Tennis is played on various surfaces including clay, grass, and hard courts."},
+    {"text": "Grapes can be eaten fresh or used to make wine."},
+    {"text": "Rivers are natural flowing watercourses that usually lead to an ocean, sea, or lake."},
+    {"text": "Basketball is a fast-paced game played with a hoop and a ball."},
+    {"text": "Cherries are small, round fruits that can be sweet or tart."},
+    {"text": "Forests are dense collections of trees and undergrowth."},
+    {"text": "Baseball is known as America's pastime."},
+    {"text": "Pineapples have a unique sweet and tangy flavor."},
+    {"text": "Volcanoes are openings in the Earth's crust that allow molten rock to escape."},
+    {"text": "Hockey is played on ice with skates and a puck."},
+    {"text": "Strawberries are bright red fruits that are often used in desserts."},
+    {"text": "Canyons are deep gorges typically carved by rivers over time."}
 ]
 
 
@@ -22,8 +38,7 @@ datasetVecs = list(dataset.get_embeddings())
 
 normVec1 = norm(queryVec)
 
-randomHashVecArray = np.random.rand(50, 384)
-print(f"Random Hash Vector Array: {randomHashVecArray}")
+randomHashVecArray = np.random.rand(24, 384)
 
 QueryHash = np.dot(queryVec, randomHashVecArray.T)
 print(f"Query Hash: {QueryHash}")
@@ -32,7 +47,7 @@ bianaryQueryHash = (QueryHash > 0).astype(int)
 print(f"Binary Query Hash: {bianaryQueryHash}")
 
 datasetHashArray = np.dot(datasetVecs, randomHashVecArray.T)
-print(f"Dataset Hash Array: {datasetHashArray}")
+
 binaryDatasetHashArray = (datasetHashArray > 0).astype(int)
 print(f"Binary Dataset Hash Array: {binaryDatasetHashArray}")
 
@@ -53,7 +68,7 @@ if best_match_idx != -1:
     print(f"Hamming distance: {min_hamming_dist}")
 
 
-# Loop over each document and calculate similarity
+# Loop over each document and calculate similarity (brute force)
 for idx, item in enumerate(dataset.get_data()):
     dotProd = np.dot(queryVec, datasetVecs[idx])
     normVec2 = norm(datasetVecs[idx])
