5ae3198e6e3d7561692561d661798bc7802a7df2
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2026-01-24 00:26:44 -0500

    primative initial vecotr db
    it works!!

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-vec1 = [[1,1,1,1],[2,5,8,1],[5,3,1,8],[1,0,0,1]]
+import dataset
+import numpy as np
+from numpy.linalg import norm
+from sentence_transformers import SentenceTransformer
 
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+dataset = dataset.Dataset(data=[{"text": "Example sentence 1."}, {"text": "Example sentence 2."}])
+
+userQuery = input("Enter your query: ")
+
+queryVec = model.encode(userQuery,convert_to_tensor=True)
+
+# Loop over each document and calculate similarity
+for idx, item in enumerate(dataset.get_data()):
+    dotProd = np.dot(queryVec, list(dataset.get_embeddings())[idx])
+    normVec1 = norm(queryVec)
+    normVec2 = norm(list(dataset.get_embeddings())[idx])
+    
+    similarity = dotProd / (normVec1 * normVec2)
+    
+    print(f"Text: {item['text']}")
+    print(f"Similarity: {similarity}")
+    print()
