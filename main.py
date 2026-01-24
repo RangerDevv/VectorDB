3810c7b1b9319583d25f67eae877b81762cedc87
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2026-01-24 00:41:46 -0500

    better optimization

--- main.py
+++ main.py
@@ -4,18 +4,29 @@ from numpy.linalg import norm
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
+data = [
+    {"text": "The sky is blue and vast."},
+    {"text": "An orange is a type of citrus fruit."},
+    {"text": "Formula 1 is the pinnacle of motorsport."},
+    {"text": "The ocean covers most of the Earth's surface."},
+    {"text": "Apples are a popular and healthy snack."}
+]
 
-dataset = dataset.Dataset(data=[{"text": "Example sentence 1."}, {"text": "Example sentence 2."}])
+
+dataset = dataset.Dataset(data=data)
 
 userQuery = input("Enter your query: ")
 
-queryVec = model.encode(userQuery,convert_to_tensor=True)
+queryVec = model.encode(userQuery)
+datasetVecs = list(dataset.get_embeddings())
+
+normVec1 = norm(queryVec)
+
 
 # Loop over each document and calculate similarity
 for idx, item in enumerate(dataset.get_data()):
-    dotProd = np.dot(queryVec, list(dataset.get_embeddings())[idx])
-    normVec1 = norm(queryVec)
-    normVec2 = norm(list(dataset.get_embeddings())[idx])
+    dotProd = np.dot(queryVec, datasetVecs[idx])
+    normVec2 = norm(datasetVecs[idx])
     
     similarity = dotProd / (normVec1 * normVec2)
     
